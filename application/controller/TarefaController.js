515b4b72e02ffd3e19e65a40212edf2d5cbc856d
Ununpentium <ununpentium@outlook.com.br>
2020-08-16 01:39:35 -0300

    PLGTAR-581620200814

--- application/controller/TarefaController.js
+++ application/controller/TarefaController.js
@@ -14,6 +14,18 @@ class TarefaController {
         }
     }
 
+    async update(request, response) {
+        try {
+            await TarefaModel.findByIdAndUpdate( { "_id": request.params.id }, request.body, { new: true } )
+                .then(responseController => {
+                    return response.status(200).json(responseController);
+                });
+        } catch (error) {
+            console.log("Erro ao tentar alterar os dados:" + error);
+            return response.status(400).json(error);
+        }
+    };
+
 }
 
 module.exports = new TarefaController();
--- application/router/TarefaRouter.js
+++ application/router/TarefaRouter.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const TarefaController = require("../controller/TarefaController");
 const TarefaMiddleware = require("../middleware/TarefaMiddleware");
+const TarefaModel = require("../model/TarefaModel");
 
 router.post("/", TarefaMiddleware, TarefaController.persist);
+
+router.put("/:id", TarefaController.update);
 
 module.exports = router;
